e80f8644e8016c7dfe7c754762c87e5e16334093
RubenSanzManovel <rubensanz100@gmail.com>
2025-12-01 16:26:09 +0100

    443

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,6 @@
 # config.py
 # Archivo de configuración para el dni con librerias y variables globales
-UDP_PORT = 6666
+UDP_PORT = 443
 SERVICE_TYPE = "_dni-im._udp.local."
 PKCS11_LIB_PATH = r"C:\Program Files\OpenSC Project\OpenSC\pkcs11\opensc-pkcs11.dll" 
 SLOT_INDEX = 0 
